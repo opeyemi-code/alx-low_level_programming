0d3b49cb0287861b9c513613c9df007ee404d171
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-17 08:03:39 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Prints out all single digit numbes from base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	return (0);
+}
