d0abbc402edb040ff25baaeffec9d2c48c8816ca
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-17 07:36:02 -0700

    fixed typo error form the else block of code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,23 +12,23 @@
 int main(void)
 {
 	int n;
-	int lastDigit;
+	int lstDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	lstDigit = n % 10;
 	/* your code goes there */
-	if (lastDigit > 5)
+	if (lstDigit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lstDigit);
 	}
-	else if (lastDigit == 0)
+	else if (lstDigit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lstDigit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstDigit);
 	}
 	return (0);
 }
