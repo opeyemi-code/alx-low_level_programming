db68a0e05d6f017f49c48715475be4384ac320ae
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-20 14:04:25 -0700

    modified

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -16,14 +16,3 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 }
-/**
- * main - Entry point
- * this function calss the print_alphabet function
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
