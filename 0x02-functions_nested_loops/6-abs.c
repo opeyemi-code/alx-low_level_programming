067c0db47204cddc0ecb59e154493c103302d9f5
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-21 11:37:06 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - Entry point
+ * @x: takes an integer as arguement
+ * this program prints out the absolute value of any number
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int x)
+{
+	return (abs(x));
+}
