76e6fa03384ededb8236a5c6f2187f4de57dc395
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-22 06:47:59 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: receives an integer
+ * @b: receives an interger
+ * returns the product of a and b
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
