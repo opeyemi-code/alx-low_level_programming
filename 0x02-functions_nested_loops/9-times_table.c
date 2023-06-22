a31b75aa780b8eb7e6fd75265a1057709f4df92a
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-21 17:17:06 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 9; j++)
+		{
+			if (j == 0)
+			{
+				printf("%d", i * j);
+			}
+			else
+			{
+				printf(", %d", i * j);
+			}
+		}
+		printf("\n");
+	}
+}
