f52172a949196eb867cfd15027f67dbfcdc16bec
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-24 10:20:52 -0700

    3-print_remaining_days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,8 +24,17 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else if ((year % 4 == 0 || year % 400) && (year % 100 == 0))
 	{
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+		if (month == 2 && day == 60)
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+		}
+		else
+		{
+			day++;
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+		}
 	}
 	else
 	{
