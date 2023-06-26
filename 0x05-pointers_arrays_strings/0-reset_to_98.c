1ae833dbd2fc6d7f19c17f1f698810cf8d29908c
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-26 14:04:33 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 -iEntry point
+ * @n: takes a pointer n that points to an integer
+ * this program updates the value n points to
+ * Return: integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
