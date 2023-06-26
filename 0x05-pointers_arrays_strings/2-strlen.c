ab7480f2e3de895bc3348c85433d92f8e9a75e22
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-26 14:38:08 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: takes a pointer argument that points to a string
+ * this program returns the lenght of a string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 void reset_to_98(int *n);
+int _strlen(char *s);
