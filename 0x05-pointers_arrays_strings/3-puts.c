2e86d473d211707d05ee2618e95732e5bf6aecdb
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-26 16:59:00 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * @str: receives a pointer that points to a string
+ * this prograam prints a string to the stdout
+ * Return: str
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
