e0273fa15782f7a7505b1c24f5f6ae3c67ca70ad
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-26 20:13:49 -0700

    9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - Entry point
+ * @dest: receives a string
+ * @src: receives a string
+ * Return: a new String
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
