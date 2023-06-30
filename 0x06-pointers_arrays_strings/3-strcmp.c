da579c166a5af0e4ad6f155cf9f904a10f4f6838
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-29 17:38:16 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - Entry point
+ * @s1: represents string1
+ * @s2: represents string2
+ * this program compares two strings
+ * Return: integer;
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
