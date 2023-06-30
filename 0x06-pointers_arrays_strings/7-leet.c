7df2022050aff81fd620245930a4b484bf55a45c
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-06-30 16:45:57 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * leet - Entry point
+ * @str: receives a string
+ * this program replaces some letters in astring with numbers
+ * Return: String
+ */
+
+char *leet(char *str)
+{
+	int str_length = strlen(str);
+	int i;
+
+	for (i = 0; i < str_length; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		}
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+		else
+		{
+			str[i] = str[i];
+		}
+	}
+	return (str);
+}
