6835224d928aa08320f150f2a06ee41cb78a2bc8
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-08-31 16:49:23 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry point
+ * @x: int
+ * @y: int
+ * this program returns x raise to power of y
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
