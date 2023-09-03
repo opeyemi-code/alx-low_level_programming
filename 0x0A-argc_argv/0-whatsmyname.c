792558bcc2a9833e27fa17069bd9ca87c217a92f
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-09-03 11:34:05 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: an integer
+ * @argv: an array of pointer to string
+ * 
+ * Return: an integer
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
