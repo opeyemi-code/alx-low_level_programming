9f938e4a6bbd15afd7ac80e7332e976e757eaba9
Opeyemi Obatola <opeyemiobatola@gmail.com>
2023-09-04 09:23:01 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	int result;
 
-	if (argc < 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
@@ -21,8 +21,8 @@ int main(int argc, char *argv[])
 	else
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
 	}
-	printf("%d\n", result);
 	return (0);
 }
 
